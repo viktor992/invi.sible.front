42f8ece88787b1131699b19e092bad637696e8bb
vughelli <vduk225@gmail.com>
2018-03-09 11:28:47 -0300

    Consultas a servicios de invi.sible.link
    Se realizan consultas a servicios de invi.sible.link
    y se procesan los datos obtenidos.
    
    Signed-off-by: vughelli <vduk225@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <h1>invi.sible.front</h1>
-    <p>Consultando datos de <a href="http://localhost:7000/api/v1/mixed/gobwebsecpy">http://localhost:7000/api/v1/mixed/gobwebsecpy</a> </p>
+    <p>Consultando datos de <a href="https://invi.sible.link">https://invi.sible.link</a> </p>
     <p>Verificar datos en consola del navegador</p>
+    {{serverData}}
   </div>
 </template>
 
@@ -11,56 +12,143 @@ export default {
   name: 'app',
   data () {
     return {
-      countries: [{short:'py', name:'paraguay'},
-      {short:'br', name:'brasil'},
-      {short:'co', name:'colombia'},
-      {short:'cl', name:'chile'}],
+      countries: [{short:'py', name:'paraguay',campaign:'gob.paraguay'},
+      {short:'br', name:'brasil',campaign:'gob.brasil'},
+      // {short:'co', name:'colombia',campaign:'gob.colombia'},
+      {short:'cl', name:'chile',campaign:'gob.chile'}
+      ],
       serverData: []
     }
   }, 
   methods: {
-    getData(){
-      this.$http.get('http://localhost:7000/api/v1/mixed/gobwebsecpy')            
-
+    getMixedData(campaign){
+      // this.$http.get('http://localhost:7000/api/v1/mixed/gobwebsecpy')            
+      let country = _.find(this.countries, x=>{return x.campaign == campaign});
+      this.$http.get(`https://invi.sible.link/api/v1/mixed/${campaign}`)            
           .then(response=>{ return response.json()})
           .then(data => {
             console.log("Original data",data)
-            console.log("Mostrando javascripts/cookies/companies por cada url/pais:",this.processData1(data))
-            console.log("Mostrando inclusion/pais",this.processData2(data))
+            console.log("Mostrando javascripts/cookies/companies por cada url/pais:",this.processData1(data, country))
+            console.log("Mostrando inclusion/pais",this.processData2(data,country))
             
             //Se guarda en variable serverData para algún uso futuro
-            this.serverData = this.processData1(data);
+            this.serverData.push(this.processData1(data,country));
 
 
           } )
     },
+
+    getSurfaceData(campaign){
+      let country = _.find(this.countries, x=>{return x.campaign == campaign});      
+      this.$http.get(`https://invi.sible.link/api/v1/surface/${campaign}`)            
+          .then(response=>{ return response.json()})
+          .then(data => {
+            console.log("Original data",data)
+            console.log("processLeaders:",this.processLeaders(data, country))
+            
+            //Se guarda en variable serverData para algún uso futuro
+            // this.serverData.push(this.processLeaders(data, country));
+            this.serverData = this.serverData.concat(this.processLeaders(data, country));
+
+          } )
+    },
+
+
+    getDetailsData(campaign){
+      let country = _.find(this.countries, x=>{return x.campaign == campaign});      
+      this.$http.get(`https://invi.sible.link/api/v1/details/${campaign}`)            
+          .then(response=>{ return response.json()})
+          .then(data => {
+            console.log("Original data",data)
+            
+            //Se guarda en variable serverData para algún uso futuro
+            this.serverData.push(this.processInclusions(data,country));
+
+
+          } )
+    },
+
+    getExtendedData(campaign){
+      let country = _.find(this.countries, x=>{return x.campaign == campaign});      
+      this.$http.get(`https://invi.sible.link/api/v1/extended/${campaign}`)            
+          .then(response=>{ return response.json()})
+          .then(data => {
+            console.log("Original data",data)
+
+            //Se guarda en variable serverData para algún uso futuro
+            this.serverData.push(this.processContentTypeExtended(data, country));
+
+
+          } )
+    },
+
+
     /**
      * Mostrar javascripts/cookies/companies por cada url/pais 
      */
-    processData1(data){
-      return _.chain(data[0]).map(x=>{return {href:x.href, cookies:x.cookies.length, javascripts:x.javascripts, companies:x.companies, country: this.getName(x.href)}}).value();
+    processData1(data,country){
+      return _.chain(data[0])
+                .map(x=>{return {href:x.href, cookies:x.cookies.length, javascripts:x.javascripts, companies:x.companies, country: country.name}})
+                .value();
     },
     /**
      * Mostrar inclusion/pais 
     */
-    processData2(data){
-      return _.chain(data[1]).map(x=>{return {href:x.href, inclusion:x.inclusion, country: this.getName(x.href)}}).value();
+    processData2(data,country){
+      return _.chain(data[1])
+                .map(x=>{return {href:x.href, inclusion:x.inclusion, country: country.name}})
+                .value();
     },
-    /**
-     * Método para obtener el nombre de un país a partir de la url
-     */
-    getName(href){
-      let name = "";
-      _.forEach(this.countries, d => {
-        if(href.includes("."+d.short+"/") || href.endsWith("."+d.short)){
-          name = d.name;
-        }
-      });
-      return name;
+    processLeaders(data,country){
+      return _.chain(data)
+                .map(x=>{return x.leaders})
+                .flatten()
+                .uniqBy(x=>x.company)
+                .sortBy(x=>x.p)
+                .map(x=>{x.siteCountry = country.name; return x})
+                .value();
+    },
+    processSiteInclusions(data,country){
+      return _.chain(data)
+                .map(x=>{x.siteCountry = country.name; return x})
+                .value();
+    },
+    processInclusions(data,country){
+      return _.chain(data)
+                .countBy(x=>x.inclusion)
+                .map((x,y)=>{return {country: country.name, inclusion:y, quantity: x, percentage: x/data.length*100}})
+                .flatten()
+                .value();
+    },
+    processSiteExtended(data,country){
+      return _.chain(data)
+                .map(x=>{x.siteCountry = country.name; return x})
+                .value();
+    },
+    processExtended(data,country){
+      return _.chain(data)
+                .countBy(x=>x.inclusion)
+                .map((x,y)=>{return {country: country.name, inclusion:y, quantity: x, percentage: x/data.length*100}})
+                .flatten()
+                .value();
+    },
+    processContentTypeExtended(data,country){
+      return _.chain(data)
+                // .countBy(x=>x["Content-Type"])
+                // .countBy(x=>x["Content-Type"] == undefined ? "UNKNOWN" : x["Content-Type"].split(';')[0])
+                .countBy(x=>x["Content-Type"] == undefined ? "UNKNOWN" : x["Content-Type"].split(';')[0].split('/')[1])
+                .map((x,y)=>{return {country: country.name, inclusion:y, quantity: x, percentage: x/data.length*100}})
+                .flatten()
+                .sortBy(x=>-x.quantity)
+                .value();
     }
   },
   mounted:function(){
-    this.getData();
+    let campaigns = _.map(this.countries, x => {return x.campaign});
+    _.forEach(campaigns, (campaign) => {
+      this.getDetailsData(campaign);
+    })    
+    
   }
 }
 </script>
